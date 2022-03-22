383e141c709099982a19547403c8d8c8d147ce5b
Your Name <vipa4ka@gmail.com>
2022-03-22 14:28:31 +0200

    cw-23

--- 23.js
+++ 23.js
@@ -1,35 +1,122 @@
-function formatDuration(seconds) {   
-
+function formatDuration(seconds) {
+    
 if (seconds === 0) {
         return 'now'
     }
-
     if (seconds < 60) {
-        return seconds + ' seconds '
+       return seconds===1 ? seconds+' second' : seconds+' seconds' 
     }
-    if (seconds <3600 ) {
-        return parseInt(seconds / 60) + ' minutes and ' +  (seconds % 60) + ' seconds'
+    if (seconds < 3600) {
+        let min;
+        parseInt(seconds / 60) === 1 ? min = "1 minute " : min = parseInt(seconds / 60) + " minutes "
+        let sec = seconds % 60;
+        if (sec === 0) {
+             sec=''
+        } else if (sec === 1) {
+            sec=sec +' second '
+        } else {
+            sec=sec+ ' seconds '
+        }   
+
+        return min  +  sec
     }
-    if (seconds <86400 ) {
-     return   parseInt(seconds / 3600) + ' hours, '+ parseInt(seconds % 3600/60) + ' minutes and ' +  (seconds % 60) + ' seconds'
+    if (seconds < 86400) { 
+        let s = seconds % 60;
+        if (s === 0) {
+             s=''
+        } else if (s === 1) {
+            s=s +' second '
+        } else {
+            s=s+ ' seconds '
+        }
+        
+        let m = parseInt(seconds % 3600 / 60);
+        if (m === 0) {
+             m=''
+        } else if (m === 1) {
+            m=', '+m +' minute and '
+        } else {
+            m=', '+m+ ' minutes and '
+        }
+        let h = parseInt(seconds / 3600);
+      let hh=   h===1 ? h+' hour' : h+' hours'
+     return  hh + m +  s
     }
-    if (seconds <31536000 ) {
-     return   parseInt(seconds / 3600/24) + ' days, '+parseInt(seconds % 3600/60/24) + ' hours, '+ parseInt(seconds % 3600/60) + ' minutes and ' +  (seconds % 60) + ' seconds'
+    if (seconds < 31536000) {
+          let d = parseInt(seconds / 3600/24);
+        let dd = d === 1 ? d + ' day' : d + ' days'
+        let hrs = parseInt(seconds % 3600 / 60/24);
+        if (hrs === 0) {
+             hrs=''
+        } else if (hrs === 1) {
+            hrs=', '+hrs +' hour'
+        } else {
+            hrs=', '+hrs+ ' hours'
+        }
+        let mmm = parseInt(seconds % 3600/60);
+        if (mmm === 0) {
+             mmm=''
+        } else if (mmm === 1) {
+            mmm=', '+mmm +' minute '
+        } else {
+            mmm=', '+mmm+ ' minutes '
+        }
+
+        let sss = seconds % 60;
+        if (sss === 0) {
+             sss=''
+        } else if (sss === 1) {
+            sss='and '+sss +' second'
+        } else {
+            sss='and '+sss+ ' seconds'
+        }
+     return  dd  +hrs+ mmm  +  sss
     } else {
-         return parseInt(seconds /365/24/ 3600) + ' years, '+  parseInt((seconds-(parseInt(seconds /365/24/ 3600))*365*24*60*60)/3600/24) + ' days, ' +parseInt((seconds-(parseInt(seconds /365/24/ 3600))*365*24*60*60)/3600/24) + ' hours, '+ parseInt(seconds % 3600/60) + ' minutes and ' +  (seconds % 60) + ' seconds'
+          let y = parseInt(seconds / 86400/365);
+        let yy = y === 1 ? '1 year, ' : y + ' years, '
+         let ddd = parseInt((seconds/86400%365));
+        if (ddd === 0) {
+             ddd=''
+        } else if (ddd === 1) {
+            ddd='1 days, '
+        } else {
+            ddd=ddd+ ' days, '
+        }
+        let hhh = parseInt(parseInt(seconds / 3600 % 24)) ;
+        if (hhh === 0) {
+             hhh=''
+        } else if (hhh === 1) {
+            hhh=', '+hhh +' hour '
+        } else {
+            hhh=', '+hhh+ ' hours '
+        }
+         let mmmm = parseInt(seconds % 3600/60);
+        if (mmmm === 0) {
+             mmmm=''
+        } else if (mmmm === 1) {
+            mmmm=', '+mmmm +' minute '
+        } else {
+            mmmm=', '+mmmm+ ' minutes '
+        }
+        let ssss = seconds % 60;
+        if (ssss === 0) {
+             ssss=''
+        } else if (ssss === 1) {
+            ssss='and '+ssss +' second '
+        } else {
+            ssss='and '+ssss+ ' seconds '
+        }  
+        return yy + ddd +hhh + mmmm + ssss        
     } 
-    
-
-
 }
 
-// console.log(formatDuration(1));   //, "1 second");
+console.log(formatDuration(1));   //, "1 second");
 // console.log(formatDuration(62));  //, "1 minute and 2 seconds");
 // console.log(formatDuration(120));  //, "2 minutes");
 // console.log(formatDuration(3600));   //, "1 hour");
 // console.log(formatDuration(3662));    //, "1 hour, 1 minute and 2 seconds");
 // console.log(formatDuration(15731080));    //, "182 days, 1 hour, 44 minutes and 40 seconds");
-console.log(formatDuration(132030240));   //, "4 years, 68 days, 3 hours and 4 minutes");
+// console.log(formatDuration(132030240));   //, "4 years, 68 days, 3 hours and 4 minutes");
 
 
 
